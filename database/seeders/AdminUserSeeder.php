87d537268d2654e45173f015ba9e492e1cd0099c
jeffry <jeffryacai@gmail.com>
2024-02-20 22:35:14 +0700

    Admin user seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -22,8 +22,54 @@ class AdminUserSeeder extends Seeder
             'email_verified_at' => date('Y-m-d H:i:s', time()),
             'password' => Hash::make('Ukrida123!'),
         ],
-        
+
+
 
     );
+
+    $userList = [
+        [
+            'name'=>'admin',
+            'email'=> 'admin@laracamp.com',
+            'is_admin' => true,
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => Hash::make('Ukrida123!'),
+        ],
+        [
+            'name'=>'Triani',
+            'email'=> 'triani.502019015@civitas.ukrida.ac.id',
+            'is_admin' => true,
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => Hash::make('Ukrida123!'),
+        ],
+        [
+            'name'=>'Samsul',
+            'email'=> 'samsul.502019028@civitas.ukrida.ac.id',
+            'is_admin' => true,
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => Hash::make('Ukrida123!'),
+        ],
+        [
+            'name'=>'Nehemia',
+            'email'=> 'nehemia.502021072@civitas.ukrida.ac.id',
+            'is_admin' => true,
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => Hash::make('Ukrida123!'),
+        ],
+        [
+            'name'=>'Brilliana',
+            'email'=> 'brilliana.502021106@civitas.ukrida.ac.id',
+            'is_admin' => true,
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => Hash::make('Ukrida123!'),
+        ],
+        [
+            'name'=>'Ria ',
+            'email'=> 'ria.502021080@civitas.ukrida.ac.id',
+            'is_admin' => true,
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => Hash::make('Ukrida123!'),
+        ],
+        ];
     }
 }
