0a0a8ab6b63cc812b6e6d2486981d361b2e294f7
jeffry <jeffryacai@gmail.com>
2024-02-21 11:42:29 +0700

    Revision footer section UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
 </div>
 
 <!-- Footer -->
-<footer class="footer" style="position: fixed;">
+<footer class="footer">
     <a
         href="https://ukrida.ac.id/studyprogram/ps/50/program-studi-psikologi"
         class="text-decoration-none text-light"
